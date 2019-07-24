aac4b0eec4fcc3b94814d3ccb4b7c2aed7f86b8b
chenxuankai <bos@itheima.com>
2019-07-24 21:50:47 +0800

    小更改

---
+++ src/main/java/com/supconit/controller/DriverController.java
@@ -0,0 +1,19 @@
+package com.supconit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: chenxuankai
+ * @Date: 2019年07月23日 11:07:52
+ * @Description: 车主controller
+ * @Version: 1.0.0
+ */
+@RestController
+@RequestMapping("/driver")
+public class DriverController {
+
+
+
+}
+
---
+++ src/main/java/com/supconit/dao/domain/PublishMsg.java
@@ -0,0 +1,16 @@
+package com.supconit.dao.domain;
+
+import lombok.Data;
+
+/**
+ * @Author: chenxuankai
+ * @Date: 2019年07月24日 21:24:19
+ * @Description: 乘客发布信息的参数实体，参数暂时没时间写全，统一调试的时候再来补全
+ * @Version: 1.0.0
+ */
+@Data
+public class PublishMsg {
+
+    private String note;
+
+}
