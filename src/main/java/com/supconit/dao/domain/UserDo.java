879ed44e7ce16cf4815bbdbb5eb884d0f79d63ae
huangmoumou123 <holidaywqq@163.com>
2019-07-24 08:43:59 +0800

    fix

--- src/main/java/com/supconit/dao/domain/UserDo.java
+++ src/main/java/com/supconit/dao/domain/UserDo.java
@@ -12,15 +12,19 @@ import lombok.Data;
 public class UserDo {
 
     private Long id;
+    private Integer isDelete;
+    private Data gmtCreate;
+    private Data gmtModify;
+    private String creater;
+    private String modifier;
     private String userName;
+    private String nickName;
     private Long mobile;
     private String avatar;
     private Integer sex;
+    private String email;
     private String openId;
     private String password;
-    private String promotionCode;
-    private String InvitationCode;
-    private String clientAssertion;
-    private String code;
-
+    private String thirdSessionId;
+    public String code;
 }
