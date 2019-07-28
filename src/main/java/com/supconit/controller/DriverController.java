41ecb00e6b5a15ee8a999fb2996adc17c1ed5d86
chenxuankai <bos@itheima.com>
2019-07-29 00:16:06 +0800

    接口基本整理完成，接下来就是细化每个接口了

--- src/main/java/com/supconit/controller/DriverController.java
+++ src/main/java/com/supconit/controller/DriverController.java
@@ -1,5 +1,8 @@
 package com.supconit.controller;
 
+import com.supconit.dao.domain.PublishMsg;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/driver")
 public class DriverController {
 
-
+    /**
+     * 司机发布信息接口
+     * */
+    @PostMapping("/publishMsg")
+    public void publishMsg(@RequestBody PublishMsg publishMsg) {
+        System.out.println(publishMsg);
+    }
 
 }
 
