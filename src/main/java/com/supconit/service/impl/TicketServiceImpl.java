efb01838171e1f0d59626352a893346246747831
huangqiujun <huangqiujun.supcon.com>
2019-08-09 16:54:42 +0800

    bug fix

--- src/main/java/com/supconit/service/impl/TicketServiceImpl.java
+++ src/main/java/com/supconit/service/impl/TicketServiceImpl.java
@@ -15,6 +15,7 @@ public class TicketServiceImpl implements TicketService {
     private TicketMapper ticketMapper;
     @Override
     public int getTicket1() {
+        System.out.println("方法内部，当前线程："+Thread.currentThread().getName());
         TicketDo ticketDo = ticketMapper.getTicketById(1l);
         int stock = ticketDo.getStock();
         if(stock > 0){
@@ -22,43 +23,40 @@ public class TicketServiceImpl implements TicketService {
             ticketDo.setStock(stock);
             return ticketMapper.updateById(ticketDo);
         }
-        return 0;
+        return -1;
     }
 
 
     public int test1() throws InterruptedException {
-        CountDownLatch countDown = new CountDownLatch(1);
         CountDownLatch await = new CountDownLatch(100);
 
         // 依次创建并启动处于等待状态的5个MyRunnable线程
         for (int i = 0; i < 100; ++i) {
-            new Thread(new MyRunnable(countDown, await)).start();
+            new Thread(new MyRunnable(await)).start();
+            await.countDown();
+            System.out.println("当前线程111："+Thread.currentThread().getName() + ",count:" + await.getCount());
         }
-
         System.out.println("用于触发处于等待状态的线程开始工作......");
         System.out.println("用于触发处于等待状态的线程工作完成，等待状态线程开始工作......");
-        countDown.countDown();
-        await.await();
+
         return 1;
     }
 
     public class MyRunnable implements Runnable {
-
-        private final CountDownLatch countDown;
         private final CountDownLatch await;
 
-        public MyRunnable(CountDownLatch countDown, CountDownLatch await) {
-            this.countDown = countDown;
+        public MyRunnable( CountDownLatch await) {
             this.await = await;
         }
 
         public void run() {
             try {
-                countDown.await();//等待主线程执行完毕，获得开始执行信号...
+                await.await();
+                //countDown.await();//等待主线程执行完毕，获得开始执行信号...
                 int result = getTicket1();
-                System.out.println("当前线程："+Thread.currentThread().getName()+",result:" + result);
-                await.countDown();//完成预期工作，发出完成信号...
-            } catch (InterruptedException e) {
+                //完成预期工作，发出完成信号...
+                System.out.println("当前线程："+Thread.currentThread().getName()+",result:" + result + ",count:" + await.getCount());
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
